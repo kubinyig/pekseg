8563256fa124836a05db4220976203e602d641b2
Kubinyi Gábor <kubinyi.gabor.21i@egressy.info>
2024-10-01 09:04:41 +0200

    Add project files.

---
+++ pekseg/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace pekseg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.namebox = new System.Windows.Forms.TextBox();
+            this.pricebox = new System.Windows.Forms.TextBox();
+            this.stockbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.listBox1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(227, 47);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(148, 134);
+            this.listBox1.TabIndex = 0;
+            // 
+            // namebox
+            // 
+            this.namebox.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.namebox.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.namebox.Location = new System.Drawing.Point(107, 45);
+            this.namebox.Name = "namebox";
+            this.namebox.Size = new System.Drawing.Size(100, 20);
+            this.namebox.TabIndex = 1;
+            // 
+            // pricebox
+            // 
+            this.pricebox.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.pricebox.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.pricebox.Location = new System.Drawing.Point(107, 103);
+            this.pricebox.Name = "pricebox";
+            this.pricebox.Size = new System.Drawing.Size(100, 20);
+            this.pricebox.TabIndex = 2;
+            // 
+            // stockbox
+            // 
+            this.stockbox.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.stockbox.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.stockbox.Location = new System.Drawing.Point(107, 161);
+            this.stockbox.Name = "stockbox";
+            this.stockbox.Size = new System.Drawing.Size(100, 20);
+            this.stockbox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(36, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Név";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(36, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ár";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(36, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Mennyiség";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(83, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Létrehozás";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(218, 237);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Törlés";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(396, 288);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.stockbox);
+            this.Controls.Add(this.pricebox);
+            this.Controls.Add(this.namebox);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox namebox;
+        private System.Windows.Forms.TextBox pricebox;
+        private System.Windows.Forms.TextBox stockbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ pekseg/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace pekseg
+{
+    public partial class Form1 : Form
+    {
+        databaseHandler db = new databaseHandler();
+        public Form1()
+        {
+            InitializeComponent();
+            start();
+        }
+        void start()
+        {
+            updateitemslist();
+            button1.Click += (s, e) =>
+            {
+                if(namebox.Text.Length >= 3 && stockbox.Text.Length > 0 && pricebox.Text.Length > 0)
+                {
+                    item oneitem = new item();
+                    oneitem.id = listBox1.Items.Count;
+                    oneitem.name = namebox.Text;
+                    oneitem.price = Convert.ToInt32(pricebox.Text);
+                    oneitem.stock = Convert.ToInt32(stockbox.Text);
+                    db.addone(oneitem);
+                    updateitemslist();
+                }
+            };
+            button2.Click += (s, e) =>
+            {
+                db.deleteone(namebox.Text);
+                updateitemslist();
+            };
+        }
+        void updateitemslist()
+        {
+            listBox1.Items.Clear();
+            List<item> items = db.readall();
+            foreach (item item in items)
+            {
+                listBox1.Items.Add(item.name + ": " + item.price + "ft");
+            }
+        }
+
+    }
+    public class databaseHandler
+    {
+        string serverAddress;
+        string username;
+        string password;
+        string databaseName;
+        string connectionString;
+        MySqlConnection connection;
+        public databaseHandler()
+        {
+            //szerver címe
+            serverAddress = "localhost";
+            username = "root";
+            password = "";
+            databaseName = "pekseg";
+            connectionString = $"Server={serverAddress};Database={databaseName};User={username};Password={password}";
+            connection = new MySqlConnection(connectionString);
+        }
+        public List<item> readall()
+        {
+            List<item> users = new List<item>();
+            try
+            {
+                connection.Open();
+                string query = "select * from keszlet";
+                MySqlCommand command = new MySqlCommand(query, connection);
+                MySqlDataReader read = command.ExecuteReader();
+                while (read.Read())
+                {
+                    item oneuser = new item();
+                    oneuser.id = read.GetInt32(read.GetOrdinal("id"));
+                    oneuser.name = read.GetString(read.GetOrdinal("name"));
+                    oneuser.stock = read.GetInt32(read.GetOrdinal("stock"));
+                    oneuser.price = read.GetInt32(read.GetOrdinal("price"));
+                    users.Add(oneuser);
+                }
+                read.Close();
+                command.Dispose();
+                connection.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("A hiba: " + e.Message);
+            }
+            return users;
+        }
+        public void addone(item oneitem)
+        {
+                try
+                {
+                    connection.Open();
+                    string query = $"insert into keszlet (name, stock, price) values ('{oneitem.name}',{oneitem.stock},{oneitem.price})";
+                    MySqlCommand command = new MySqlCommand(query, connection);
+                    command.ExecuteNonQuery();
+                    command.Dispose();
+                    connection.Close();
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Error: " + e.Message);
+                }
+            } 
+        public void deleteone(string name)
+        {
+            try
+            {
+                connection.Open();
+                string query = $"DELETE FROM keszlet WHERE name =  '{name}'";
+                MySqlCommand command = new MySqlCommand(query, connection);
+                command.ExecuteNonQuery();
+                command.Dispose();
+                connection.Close();
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Error: " + e.Message);
+            }
+            
+        }
+    }
+    public class item{
+        public int id { get; set; }
+        public string name { get; set; }
+        public int stock { get; set; }
+        public int price { get; set; }
+    }
+}
